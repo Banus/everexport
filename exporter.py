a0a6403121c679c6254af6ec749726d59ba50fc7
Emanuele Plebani <banus80@gmail.com>
2021-11-24 22:58:16 -0500

    Support text attributes, code and subscripts, add print option
    Support complex text attributes to handle state spanning multiple tags,
    eg links or code; support sub/super scripts. Cleaning stage with
    replacement of double newlines to keep the original Evernote
    appearance.
    Add --print option to show the result on stdout.

--- exporter.py
+++ exporter.py
@@ -1,6 +1,8 @@
 """Converts Evernote HTML files to Markdown."""
 import argparse
+import os
 import re
+import sys
 from html.parser import HTMLParser
 
 
@@ -16,35 +18,54 @@ def decode_style(prop):
     style['italic'] = tokens.get('font-style', None) == 'italic'
     style['bold'] = tokens.get('font-weight', None) == 'bold'
     style['underline'] = tokens.get('text-decoration', None) == 'underline'
+    style['sub'] = tokens.get('vertical-align', None) == 'sub'
+    style['code'] = "courier" in tokens.get('font-family', "").lower()
 
     return style
 
 
-def _format(string, style):
-    """Add formats to string."""
-    if not style:
+def _format(string, attr, warn=True):
+    """Add formats to string; beware of the order."""
+    if not attr:
         return string
 
-    if style.get('underline', False):
+    if attr.get('code', False):
+        # spaces are non-breaking in code; remove leading/trailing spaces
+        string = string.replace(u"\xc2\xa0", " ").strip()
+        if not string:  # nothing left
+            if warn:
+                print("Empty code detected", file=sys.stderr)
+                return "<empty code>"
+            return ""
+        string = f"`{string}`"
+    if attr.get('sup', False):
+        string = f"<sup>{string}</sup>"
+    if attr.get('sub', False):
+        string = f"<sub>{string}</sub>"
+    if attr.get('underline', False):
         string = f"<u>{string}</u>"
-    if style.get('italic', False):
+    if attr.get('link', None):
+        string = f"[{string}]({attr['link']})"
+    if attr.get('italic', False):
         string = f"*{string}*"
-    if style.get('bold', False):
+    if attr.get('bold', False):
         string = f"**{string}**"
 
     return string
 
 
+# check: https://docs.python.org/3/library/html.parser.html
 class HTMLMDParser(HTMLParser):
-    # check: https://docs.python.org/3/library/html.parser.html
+    """Convert each HTML tag in a corresponding Markdown construct."""
 
     def __init__(self):
+        """Initialize parser for a single document."""
         super(HTMLMDParser, self).__init__()
-        self.tags, self.last = [], None
-        self.style, self.link = None, None
-        self.running, self.txt = False, ""
+        self.tags, self.last, self.span_keys = [], None, []
+        self.attr, self.running, self.txt = {}, False, ""
 
     def handle_starttag(self, tag, attrs):
+        """Handle state update for a new tag."""
         # print("Start tag:", tag)
         self.tags.append(tag)
         attrs = dict(attrs)
@@ -53,9 +74,16 @@ class HTMLMDParser(HTMLParser):
                 # hack: avoid bad syntax with two close spans
                 self.txt += " "
             if 'style' in attrs:
-                self.style = decode_style(attrs['style'])
+                self.attr = decode_style(attrs['style'])
+                self.span_keys = list(self.attr.keys())
         elif tag == 'a' and 'href' in attrs:
-            self.link = attrs['href'].replace(" ", "_")
+            self.attr['link'] = attrs['href'].replace(" ", "_")
+        elif tag == 'u':
+            self.attr['underline'] = True
+        elif tag == 'i':
+            self.attr['italic'] = True
+        elif tag == 'sup':
+            self.attr['sup'] = True
         elif tag == 'ul':
             self.txt += '\n'
         elif tag == 'li':
@@ -66,38 +94,48 @@ class HTMLMDParser(HTMLParser):
             self.running = True
 
     def handle_endtag(self, tag):
+        """Handle state update when closing a tag."""
         if self.tags[-1] != tag:
             raise ValueError(f"Unmatched tags, {tag} closing {self.tags[-1]}")
 
         if tag in ('div', 'ul'):
             self.txt += "\n"
+        elif tag == 'h1':
+            self.txt += "\n\n"
         elif tag == 'span':
-            self.style = None  # end of style block
+            self.attr = {}
         elif tag == 'a':
-            self.link = None
+            self.attr.pop('link', None)
+        elif tag == 'u':
+            self.attr.pop('underline')
+        elif tag == 'i':
+            self.attr.pop('italic')
+        elif tag == 'sup':
+            self.attr.pop('sup')
         elif tag == 'body':
             self.running = False
 
         self.last = self.tags.pop()
 
     def handle_data(self, data):
+        """Handle tag content."""
         if not self.running:
             return
+        if not data.strip():  # only spaces
+            return
 
-        if self.link:
-            data = f"[{data}]({self.link})"
-        data = _format(data, self.style)
-        self.txt += data
+        self.txt += _format(data, self.attr)
 
+    def finalize(self, cr_="<br>"):
+        """Clean up residual formatting."""
+        txt = self.txt.replace(u"\xc2\xa0", "&nbsp;")
+        txt = re.sub(r"\n{3,}", "\n\n", txt)
+        # remove lone nbsp - neded before single cr conversion
+        txt = re.sub(r"\n\**(&nbsp;)+\**\n", "\n\n", txt)
+        # explicit single returns except for bullet lists
+        txt = re.sub(r"(\S)\n([^\n-])", rf"\1{cr_}\n\2", txt)
 
-def _finalize(txt, cr_="<br>"):
-    """Clean up residual formatting."""
-    txt = txt.replace(u"\xc2\xa0", "&nbsp;")
-    txt = re.sub(r"\n{3,}", "\n\n", txt)
-    # explicit single returns except for bullet lists
-    txt = re.sub(r"(\S)\n([^[\n-])", rf"\1{cr_}\n\2", txt)
-
-    return txt.strip() + '\n'
+        return txt.strip() + '\n'
 
 
 def main():
@@ -105,20 +143,33 @@ def main():
     parser = argparse.ArgumentParser(
         description='Convert Evernote HTML to Markdown')
 
-    parser.add_argument('path', type=str, help="file to convert")
+    parser.add_argument("path", type=str, help="file to convert")
+    parser.add_argument("-p", "--print", action="store_true",
+                        help="print to stdout instead of writing files.")
     args = parser.parse_args()
 
     with open(args.path, 'r') as fid:
         txt = '\n'.join(line for line in fid)
+    fname, _ = os.path.splitext(args.path)
 
     parser = HTMLMDParser()
     parser.feed(txt)
+    parser.close()
 
-    txt = _finalize(parser.txt)
-    with open("test.md", 'w') as fid:
-        fid.write(txt)
+    txt = parser.finalize(cr_=r"\\")
 
-    print(txt)
+    if args.print:
+        print(txt)
+    else:
+        with open(f"{fname}.md", 'w') as fid:
+            fid.write(txt)
+
+    # TODO: fix code block
+    # TODO: embed PDFs, videos, images
+    # <object data="x.pdf" width="1000" height="1000" type='application/pdf'/>
+    # <video muted autoplay controls>
+    # <source src="{{ site.my-media-path }}/myvideo.mp4" type="video/mp4">
+    # </video>
 
 
 if __name__ == '__main__':
