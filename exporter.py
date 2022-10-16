3c03a3ea35c6e03d983da0f8d8b0b95ff413cc0c
Emanuele Plebani <banus80@gmail.com>
2022-10-15 23:32:23 -0400

    Add several tags, support style in code
    Added support to several new HTML tags an formatting options, eg
    strikethrough; support paragraph alignment.
    Support formats inside inline code by using the HTML tag (works in
    Joplin and Obsidian).
    Add option to convert all the files in a given directory.

--- exporter.py
+++ exporter.py
@@ -6,6 +6,7 @@ import logging
 import json
 import os
 import re
+from string import whitespace
 
 from itertools import chain
 from urllib.parse import quote
@@ -13,8 +14,12 @@ from urllib.parse import quote
 from bs4 import BeautifulSoup, NavigableString
 
 
-_MONOSPACE_FONTS = ["Andale Mono", "Consolas", "Courier New", "Lucida Console",
-                    "courier new, courier, monospace"]
+_MONOSPACE_FONTS = ["andale mono", "consolas", "courier new", "lucida console",
+                    "monospace"]
+_KNOWN_FONTS = _MONOSPACE_FONTS + [
+    "arial", "calibri", "tahoma", "times new roman", "lucida sans unicode",
+    "helvetica", "verdana", "windings"]
+_WHITESPACE = tuple(whitespace) + (chr(160),)
 
 # options
 COLOR_INTERNAL_LINKS = True
@@ -28,6 +33,8 @@ def parse_span_style(tokens):
     style['italic'] = tokens.get('font-style', None) == 'italic'
     style['bold'] = tokens.get('font-weight', None) == 'bold'
     style['underline'] = tokens.get('text-decoration', None) == 'underline'
+    style['strikethrough'] = \
+        tokens.get('text-decoration', None) == 'line-through'
     style['sub'] = tokens.get('vertical-align', None) == 'sub'
     style['sup'] = tokens.get('vertical-align', None) == 'super'
     style['code'] = "courier" in tokens.get('font-family', "").lower()
@@ -37,7 +44,12 @@ def parse_span_style(tokens):
 
 
 def _parse_div_style(tokens):
-    return {'codeblock': tokens.get("-en-codeblock", "") == "true"}
+    style = {}
+    style['codeblock'] = tokens.get("-en-codeblock", "") == "true"
+    align = tokens.get('text-align', '').lower()
+    style['align'] = align if align in ('center', 'right') else ''
+
+    return {k: v for k, v in style.items() if v}  # drop useless attributes
 
 
 def _parse_font_style(tokens):
@@ -64,11 +76,13 @@ def _parse_img(tag):
 
 def _escape(string):
     """Escape reserved Markdown characters."""
-    return string.replace("<", r"\<").replace(">", r"\>").replace("$", r"\$")
+    string = string.replace("<", r"\<").replace(">", r"\>").replace("$", r"\$")
+    return string.replace("*", r"\*").replace("`", r"\`")
 
 def _unescape(string):
     """Unescape reserved Markdown characters for codeblocks."""
-    return string.replace(r"\>", ">").replace(r"\<", "<").replace(r"\$", "$")
+    string = string.replace(r"\>", ">").replace(r"\<", "<").replace(r"\$", "$")
+    return string.replace(r"\*", "*").replace(r"\`", "`")
 
 
 def decode_style(prop, parser=parse_span_style):
@@ -77,9 +91,10 @@ def decode_style(prop, parser=parse_span_style):
         return {}
 
     # occasionally, Evernote drops the font-family attribute
-    prop = re.sub("\";} \"", "\"Courier New\"", prop)
+    prop = re.sub("\"(;} |;)\"", "\"Courier New\"", prop)
     tokens = [t.strip() for t in prop.split(';') if t]
-    tokens = dict([tuple(x.strip() for x in t.split(':')) for t in tokens])
+    tokens = dict([tuple(x.strip() for x in t.split(':', maxsplit=1))
+                   for t in tokens])
     # handle aliases
     tokens['text-decoration'] = tokens.pop('text-decoration-line',
                                            tokens.get('text-decoration', None))
@@ -123,18 +138,37 @@ def _clean_code_block(string):
 
 def _format(string, attr, warn=True):
     """Add formats to string; beware of the order."""
-    if not attr or not string.strip():
+    if not attr or string.isspace():
         return string
+
+    if string.endswith(_WHITESPACE) or string.startswith(_WHITESPACE):
+        # remove formatting from leading/trailing whitespace to avoid MD errors
+        clean = string.strip(''.join(_WHITESPACE))
+        off, size = string.find(clean[0]), len(clean)
+        return string[:off] + _format(clean, attr, warn) + string[off+size:]
+    
+    if attr.get('codeblock', False):
+        lang = attr.get('lang', 'bash')
+        return f"\n```{lang}\n{_clean_code_block(string)}\n```"
+
+    if attr.get('code', ''):
+        # spaces are non-breaking in code; remove leading/trailing spaces
+        string = _clean_code_block(string)
+        if not string:
+            return ""
+
+        string = f"<code>{string}</code>" if attr['code'] == 'html' \
+            else f"`{string}`"
 
     if attr.get('color', "") == "rgb(0, 0, 0)":
         attr.pop('color')  # remove default color
+    if string and string[0] == '#' and re.match(r'#+ ', string):
+        # move headings out of formatting
+        head, string = string.split(' ', maxsplit=1)
+        head = head + ' '
+    else:
+        head = ""
 
-    if attr.get('code', False):
-        # spaces are non-breaking in code; remove leading/trailing spaces
-        string = _clean_code_block(string)
-        # no further formatting is possible
-        return f"`{string}`" if string else ""
-    
     # links in italics are normally definitions and use underline by default
     #  but log the instances we found
     if attr.get('link', None) and attr.get('italic', False):
@@ -142,22 +176,31 @@ def _format(string, attr, warn=True):
             attr['underline'] = True
             logging.debug("Link in italics without underline: %s", string)
 
+    if attr.get('abbr', None):
+        string = f"<abbr title=\"{attr['abbr']}\">{string}</abbr>"
     if attr.get('sup', False):
         string = f"<sup>{string}</sup>"
     if attr.get('sub', False):
         string = f"<sub>{string}</sub>"
     if attr.get('underline', False):
         string = f"<u>{string}</u>"
+    if attr.get('strikethrough', False):
+        string = f"~~{string}~~"
     if attr.get('color', False):
         string = f"<span style=\"color: {attr['color']}\">{string}</span>"
     if attr.get('italic', False):
         string = f"*{string}*"
     if attr.get('bold', False):
         string = f"**{string}**"
+    if attr.get('align', ''):
+        string = string.replace('\n', '<br>')  # only BR works with divs
+        string = f"<div align=\"{attr['align']}\">{string}</div>"
     if attr.get('link', None):
         string = f"[{string}]({attr['link']})"
+    if attr.get('heading', 0):
+        string = f"{'#' * attr['heading']} {string}"
 
-    return string
+    return head + string
 
 
 def _format_spans(spans):
@@ -174,8 +217,12 @@ def _format_spans(spans):
     base_style = styles[0].intersection(*styles[1:])
     styles = [dict(s.difference(base_style)) for s in styles]
 
+    base_style = dict(base_style)
+    if 'code' in base_style:
+        base_style['code'] = 'html'  # to handle formatting in code
+
     chunk = ''.join(_format(t, s) for t, s in zip(texts, styles))
-    return _format(chunk, dict(base_style))  # also apply base style
+    return _format(chunk, base_style)  # also apply base style
 
 
 def _replace_cr_except_code(txt, langs, cr_):
@@ -229,10 +276,10 @@ def finalize(txt, hints, cr_="<br>"):
     txt = re.sub(r"\n\n\n+", "\n\n", txt)
     # remove lone nbsp - needed before single cr conversion
     txt = re.sub(r"\n\**(&nbsp;)+\**\n", "\n\n", txt)
-    # # nbsp not at line start are spaces
+    # nbsp not at line start are spaces
     txt = re.sub(r"&nbsp;(?![&nbsp;| |*]+)", " ", txt)
     # remove trailing spaces
-    txt = re.sub(r" *\n", "\n", txt)
+    txt = re.sub(r" +\n", "\n", txt)
     txt = _replace_cr_except_code(txt, hints.get('codeblocks', []), cr_)
 
     return txt.strip() + '\n'
@@ -350,64 +397,78 @@ class HTMLParser():
             txt = self._merge_tags(children)
         elif len(children) == 1:
             txt, style = self.process_tag(children[0])
-            if tag.name != 'span':  # stop here
-                txt, style = _format(txt, style), {}
+            # if tag.name != 'span':  # stop here
+            #     txt, style = _format(txt, style), {}
 
         if tag.name == 'div':
             div_style = decode_style(tag.get('style', ''), _parse_div_style)
             if div_style.get('codeblock', False):
-                txt = f"\n```bash\n{_clean_code_block(txt)}\n```"
+                style['codeblock'], style['lang'] = True, 'bash'
+        elif tag.name == 'pre':  # pre-formatted text
+            style['codeblock'], style['lang'] = True, ''
         elif tag.name == 'span':
             if len(children) <= 1:
                 # if multiple children, their style overrides the parent
                 style.update(decode_style(tag.get('style', '')))
-        elif tag.name == 'h1':
-            txt = f"# {txt}"
-        elif tag.name == 'h2':
-            txt = f"## {txt}"
-        elif tag.name == 'h3':
-            txt = f"### {txt}"
-        elif tag.name == 'h4':
-            txt = f"#### {txt}"
+        elif tag.name == 'hr':  # horizontal rule
+            txt = f"\n----\n"
+        elif tag.name.startswith('h'):  # heading
+            style = decode_style(tag.get('style', ''), _parse_div_style)
+            style['heading'] = int(tag.name[1])
         elif tag.name == 'a':
             txt, style = self._parse_link(tag, txt, style)
         elif tag.name == 'img':
             txt = _parse_img(tag)
         elif tag.name == 'font':
-            font = tag.get('face', "").strip("'")
+            font = tag.get('face', "").split(',')[0].strip("'").lower()
             style['code'] = font in _MONOSPACE_FONTS or decode_style(
-                tag.get('style', ""), _parse_font_style)
-            if font and font not in _MONOSPACE_FONTS:
+                tag.get('style', ""), _parse_font_style).get('code', False)
+            if font and font not in _KNOWN_FONTS:
                 logging.warning(f"Unknown font: {font}")
+        elif tag.name == 'blockquote':
+            txt = '> ' + txt.replace('\n', '\n> ')
         elif tag.name == 'p':
             txt = f"{txt}\n"
         elif tag.name == 'br':
             txt = '\n'
-        elif tag.name == 'ul':
+        elif tag.name in ('ul', 'ol'):
             txt = f"\n{txt}"
         elif tag.name == 'li':
             mark = '- ' if tag.parent.name == 'ul' else '1. '
             txt = f"{mark}{txt}\n"
         elif tag.name == 'u':
             style['underline'] = True
+        elif tag.name in ('del', 'strike', 's'):
+            style['strikethrough'] = True
+        elif tag.name in ('code', 'tt'):
+            style['code'] = True
         elif tag.name in ('i', 'em'):
             style['italic'] = True
         elif tag.name in ('b', 'strong'):
             style['bold'] = True
+        elif tag.name == 'q':
+            txt = f'"{txt}"'
         elif tag.name == 'sup':
             style['sup'] = True
         elif tag.name == 'sub':
             style['sub'] = True
-        elif tag.name == 'table':
+        elif tag.name == 'small':
+            style['sub'], style['sup'] = True, True
+        elif tag.name == 'center':
+            style['align'] = 'center'
+        elif tag.name == 'abbr':
+            style['abbr'] = tag.get('title', ' ')
+        elif tag.name in ('table', 'tbody'):
             txt = f'\n\n{txt}\n'
         elif tag.name == 'tr':
             txt = convert_tr(tag, txt)
         elif tag.name in ('td', 'th'):
             txt = f' {txt} |'
-        elif tag.name == 'body':
-            pass   # do noting more
+        elif tag.name in ('body', 'cite', 'address'):
+            pass   # ignore tags
         else:
             logging.warning(f"Unknown tag: {tag.name}")
+
         return txt, style
 
 
@@ -427,14 +488,20 @@ def main():
         description='Convert Evernote HTML to Markdown')
 
     parser.add_argument("path", type=str, help="file to convert")
-    parser.add_argument("-r", "--recursive", action="store_true",
+    parser.add_argument("-m", "--mode", default="single",
                         help="recursively convert all files following links.")
     args = parser.parse_args()
 
     with open("hints.json", 'r') as fid:
         hints = json.load(fid)
 
-    links, converted = [os.path.split(args.path)], []
+    path = os.path.dirname(args.path)
+    all_files = sorted(f for f in os.listdir(path) if f.endswith('.html'))
+    all_files = all_files[::-1]  # to have ascending order with pop()
+    links = [(path, f) for f in all_files] \
+        if args.mode == 'all' else [os.path.split(args.path)]
+    
+    converted = []
     while links:
         path, fname = links.pop()
         logging.info(f"Converting {fname}")
@@ -448,11 +515,10 @@ def main():
             fid.write(txt)
         converted.append(fname)
 
-        if args.recursive:
+        if args.mode == 'recursive':
             links += [(path, f) for _, f in curr_links if f not in converted]
 
-    if args.recursive:
-        all_files = [f for f in os.listdir(path) if f.endswith('.html')]
+    if args.mode == 'recursive':
         print("\nMissing files:")
         for fname in set(all_files) - set(converted):
             print(f"{fname}")
